30e52c2af1f86729eccab9dda9bd18ef0c6002f9
ZelenkovKonstantin <Konstantinzelenkov9@gmail.com>
2024-09-11 22:59:39 +0300

    Add Prog4.java

---
+++ 1209/Prog4.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Prog4 {
+    public static void main(String[] args) {
+        Scanner Sc = new Scanner(System.in);
+        String name = Sc.nextLine();
+        System.out.println("Привет " + name + "!");
+    }
+}
