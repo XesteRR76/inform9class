05c5046e226a8040a2e1cc9149fb73b60fe06bf7
X_est_eRR <140378278+XesteRR76@users.noreply.github.com>
2024-09-12 14:38:39 +0300

    Update number1.java

--- EnterOnScreen/number1.java
+++ EnterOnScreen/number1.java
@@ -1,7 +1,7 @@
 import java.io.PrintStream;
 import java.util.Scanner;
 public class Main {
-    public static Scanner in = new Scanner(System.in);
+ public static void main(String[] args) {
         Scanner Sc = new Scanner(System.in);
         String name = Sc.nextLine();
         System.out.println("Hello, " + name + "!");
