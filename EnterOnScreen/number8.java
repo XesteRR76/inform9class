d3b903a31db9fa6902c37f4e9b402c69959ebeda
X_est_eRR <140378278+XesteRR76@users.noreply.github.com>
2024-09-12 14:56:28 +0300

    Create number8.java

---
+++ EnterOnScreen/number8.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        }
+    }
+}
