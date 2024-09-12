b937762e9ed6f0a42779a62ce7f1fa5ef93bb99e
X_est_eRR <140378278+XesteRR76@users.noreply.github.com>
2024-09-12 14:57:50 +0300

    Update number8.java

--- EnterOnScreen/number8.java
+++ EnterOnScreen/number8.java
@@ -1,7 +1,25 @@
+import java.io.PrintStream;
 public class Main {
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
-            System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
-        }
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
+        System.out.println("Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste Copy-Paste");
     }
 }
