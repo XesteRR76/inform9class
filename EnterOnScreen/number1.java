36c11128107f34caf25d475b63a5c2cac1edf5eb
X_est_eRR <140378278+XesteRR76@users.noreply.github.com>
2024-09-12 14:37:30 +0300

    Create number1.java

---
+++ EnterOnScreen/number1.java
@@ -0,0 +1,9 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+public class Main {
+    public static Scanner in = new Scanner(System.in);
+        Scanner Sc = new Scanner(System.in);
+        String name = Sc.nextLine();
+        System.out.println("Hello, " + name + "!");
+    }
+}
